ab445b2f43d5e0438ec6fa9bc0fca704a85dbe55
Эрик <e.arstamyan@bk.ru>
2022-07-31 18:53:22 +0300

    renamed files

--- app_functions.go
+++ app_functions.go
@@ -7,6 +7,21 @@ import (
 	"net/http"
 )
 
+func getLocation(latitude float64, longitude float64) string {
+
+	url := fmt.Sprintf("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?outSR=4326&returnIntersection=false&location=%f,%f&f=json", longitude, latitude)
+	response, err := http.Get(url)
+	checkErrors(err)
+
+	body, err := ioutil.ReadAll(response.Body)
+	checkErrors(err)
+
+	var responseObject Location
+	json.Unmarshal(body, &responseObject)
+
+	return fmt.Sprintf("%s, %s, %s", responseObject.Location.Country, responseObject.Location.City, responseObject.Location.Address)
+}
+
 func solveAnagram(word string) string {
 
 	url := fmt.Sprintf("https://anagram.poncy.ru/anagram-decoding.cgi?name=anagram_main&inword=%s&answer_type=1", word)
